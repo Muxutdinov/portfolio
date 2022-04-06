98896ed364a07a6ed9f33cbe5c303f2653cf8331
Muxutdinov <jmuxutdinov@mail.ru>
2022-04-06 17:10:07 +0500

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Jaxongir</div>
+        <div className="n-toggle">toggle</div>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{listStyleType:'none'}}>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Experience</li>
+            <li>Porfolio</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <div className="n-btn btn">Contact</div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
